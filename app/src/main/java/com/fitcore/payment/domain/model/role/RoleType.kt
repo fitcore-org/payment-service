48c9ff531297eacab43aa07172040b01aaba1b4b
yLukas077 <lukasmbs1234@gmail.com>
2025-08-07 14:54:52 -0300

    role enum adjusts

--- app/src/main/java/com/fitcore/payment/domain/model/role/RoleType.kt
+++ app/src/main/java/com/fitcore/payment/domain/model/role/RoleType.kt
@@ -1,8 +1,9 @@
 package com.fitcore.payment.domain.model
 
 enum class RoleType {
+    CLEANER,
+    PERSONAL_TRAINER,
+    RECEPTIONIST,
     STUDENT,
-    TEACHER,
-    SECRETARY,
-    ADMINISTRATOR,
+    MANAGER
 }
